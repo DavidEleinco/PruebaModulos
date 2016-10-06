dca661383af45b15110c5e4a653fb91f5b96efc5
ludagoga117 <luis.goyes@udea.edu.co>
2016-10-06 10:56:03 -0500

    Al pelo lo básico de sockets entre cliente y servidor

--- sockets_cliente/src/main/java/co/com/eleinco/sockets_cliente/MainActivity.java
+++ sockets_cliente/src/main/java/co/com/eleinco/sockets_cliente/MainActivity.java
@@ -1,15 +1,40 @@
 package co.com.eleinco.sockets_cliente;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt_enviar;
-    EditText et_texto;
+    private Button bt_enviar;
+    private Button bt_conectar;
+    private EditText et_texto;
+    private TextView tv_chat;
+    private EditText et_ipserver;
+
+    private boolean connected = false;
+
+    // Server IP
+    public static String SERVERIP = "";
+
+    // designate a port
+    public static final int SERVERPORT = 8888;
+
+    private Handler handler = new Handler();
+
+    private ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
 
         bt_enviar = (Button) findViewById(R.id.main_bt_enviar);
         et_texto = (EditText) findViewById(R.id.main_et_loquevoyadecir);
+        tv_chat = (TextView) findViewById(R.id.main_tv_chat);
+        et_ipserver = (EditText) findViewById(R.id.main_et_IPserver);
+        bt_conectar = (Button) findViewById(R.id.main_bt_conectar);
+
+        bt_conectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conectar();
+            }
+        });
+
 
         bt_enviar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +61,47 @@ public class MainActivity extends AppCompatActivity {
                 enviarTexto();
             }
         });
+    }
+
+    private void conectar(){
+        if (!connected) {
+            SERVERIP = et_ipserver.getText().toString();
+            if (!SERVERIP.equals("")) {
+                Thread cThread = new Thread(new ClientThread());
+                cThread.start();
+            }
+        }
+    }
+
+    public class ClientThread implements Runnable {
+
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVERIP);
+                Log.d("ClientActivity", "C: Connecting...");
+                Socket socket = new Socket(serverAddr, SERVERPORT);
+                connected = true;
+                while (connected) {
+                    try {
+                        Log.d("ClientActivity", "C: Sending command.");
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                                .getOutputStream())), true);
+                        // where you issue the commands
+                        out.println("Hey Server!");
+                        Log.d("ClientActivity", "C: Sent.");
+                        break;
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                connected = false;
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+        }
     }
 
     private void enviarTexto(){
--- sockets_servidor/src/main/java/co/com/eleinco/sockets_servidor/MainActivity.java
+++ sockets_servidor/src/main/java/co/com/eleinco/sockets_servidor/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,24 +17,156 @@ import org.w3c.dom.Text;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text;
-    private TextView ip_puerto;
+    private Button bt_enviar;
+    private EditText et_texto;
+    private TextView tv_chat;
+    private TextView serverStatus;
 
-    private static final int SERVERPORT = 6000;
-    private static String SERVERIP;
+    // Server IP
+    public static String SERVERIP = "";
+
+    // designate a port
+    public static final int SERVERPORT = 8888;
+
+    private Handler handler = new Handler();
+
+    private ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (TextView) findViewById(R.id.main_serverCommunication);
-        ip_puerto = (TextView) findViewById(R.id.main_IP_PORT);
+        bt_enviar = (Button) findViewById(R.id.main_bt_enviar);
+        et_texto = (EditText) findViewById(R.id.main_et_loquevoyadecir);
+        tv_chat = (TextView) findViewById(R.id.main_tv_chat);
+        serverStatus = (TextView) findViewById(R.id.main_tv_infoIP);
+
+        SERVERIP = getLocalIpAddress();
+
+        Thread fst = new Thread(new ServerThread());
+        fst.start();
+
+
+        bt_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarTexto();
+            }
+        });
+    }
+
+
+    public class ServerThread implements Runnable {
+        public void run() {
+            try {
+                if (SERVERIP != null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Listening on IP: " + SERVERIP + ":"+SERVERPORT);
+                        }
+                    });
+                    serverSocket = new ServerSocket(SERVERPORT);
+                    while (true) {
+                        // listen for incoming clients
+                        Socket client = serverSocket.accept();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                serverStatus.setText("Listening on IP: " + SERVERIP + ":"+SERVERPORT+"\nConnected.");
+                            }
+                        });
+
+                        try {
+                            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            String line = null;
+                            while ((line = in.readLine()) != null) {
+                                Log.d("ServerActivity", line);
+                                final String finalLine = line;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // do whatever you want to the front end
+                                        // this is where you can be creative
+                                        tv_chat.setText( tv_chat.getText().toString() + "\n*OTRO: "+ finalLine);
+                                    }
+                                });
+                            }
+                            //break;
+                        } catch (Exception e) {
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    serverStatus.setText("Connection interrupted.");
+                                }
+                            });
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Couldn't detect internet connection.");
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        serverStatus.setText("Error");
+                    }
+                });
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private void enviarTexto(){
+
+    }
+
+
+    // gets the ip address of your phone's network
+    private String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (inetAddress instanceof Inet4Address && !inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("ServerActivity", ex.toString());
+        }
+        return null;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            // make sure you close the socket upon exiting
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
